7040ed7815979f203ee59a5e84e7226ad47a0616
allaev <34710484+allaev@users.noreply.github.com>
2018-05-01 23:43:21 +0500

    require models

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
 
 router.get('/', function(req, res){
-    res.send("Todos route");
+    db.Todo.find()
+    .then(function(todos){
+        res.json(todos);
+    })
+    .catch(function(err){
+        res.send(err);
+    });
 });
 
 module.exports = router;
