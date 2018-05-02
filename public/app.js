a7636e24c2bd7d08c787e2e55648def08d3e46ac
allaev <34710484+allaev@users.noreply.github.com>
2018-05-02 15:35:13 +0500

    add a conditional statement for displaying todos

--- public/app.js
+++ public/app.js
@@ -7,6 +7,9 @@ function addTodos(todos){
     //add todos to the page
     todos.forEach(function(todo){
       var newTodo = $('<li class="task">'+todo.name+'</li>');
+      if(todo.completed){
+        newTodo.addClass("done");
+      }
       $('.list').append(newTodo);
     });
 }
