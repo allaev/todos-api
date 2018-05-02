e92907e4439e43654745af83ac314b1b4b2a2aba
allaev <34710484+allaev@users.noreply.github.com>
2018-05-02 17:04:45 +0500

    create removeTodo function

--- public/app.js
+++ public/app.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
         createTodo();
       }
     });
+    
+    $('.list').on('click', 'span', function(){
+      removeTodo($(this).parent());
+      // var clickedId = $(this).parent().data('id');
+      // var deleteUrl = '/api/todos/' + clickedId;
+      // $(this).parent().remove();
+      // $.ajax({
+      //   method: 'DELETE',
+      //   url: deleteUrl
+      // })
+      // .then(function(data){
+      //   console.log(data);
+      // });
+    });
 });
       
 function addTodos(todos){
@@ -17,7 +31,8 @@ function addTodos(todos){
 }
 
 function addTodo(todo){
-  var newTodo = $('<li class="task">'+todo.name+'</li>');
+  var newTodo = $('<li class="task">'+todo.name+'<span>X</span></li>');
+  newTodo.data('id', todo._id);
   if(todo.completed){
     newTodo.addClass("done");
   }
@@ -31,6 +46,21 @@ function createTodo(){
   .then(function(newTodo){
     $('#todoInput').val('');
     addTodo(newTodo);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+function removeTodo(todo){
+  var clickedId = todo.data('id');
+  var deleteUrl = '/api/todos/' + clickedId;
+  $.ajax({
+    method: 'DELETE',
+    url: deleteUrl
+  })
+  .then(function(data){
+    todo.remove();
   })
   .catch(function(err){
     console.log(err);
